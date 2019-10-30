d4ba087916d63dfcc55c30e6e1b4174a43df389d
Alan Jenkins <alan.jenkins@claranet.uk>
2019-10-30 06:38:50 +0000

    Add documentation.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "policies" {
-  value = "${module.policy_condenser.policies}"
+  description = "A list of 1 or more IAM Policy documents in JSON format. These should be used with the aws_iam_policy resource to create the policies for the user, group or role."
+  value       = "${module.policy_condenser.policies}"
 }
