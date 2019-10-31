e464ce8a51af513f21a517de5d671f2e69297264
Alan Jenkins <alan.jenkins@claranet.uk>
2019-10-31 15:53:39 +0000

    Fix typo

--- lambda.tf
+++ lambda.tf
@@ -121,7 +121,7 @@ data "aws_iam_policy_document" "lambda_write" {
   statement {
     sid = "LambdaWriteAccess"
 
-    action = [
+    actions = [
       "lambda:AddPermission",
       "lambda:CreateAlias",
       "lambda:CreateEventSourceMapping",
