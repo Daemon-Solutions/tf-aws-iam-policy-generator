4dc0d81bd005638e4b20fab383e6b152f548c788
Alan Jenkins <alan.jenkins@claranet.uk>
2019-11-04 08:52:06 +0000

    Sort policies alphabetically
    Sort the policy list alphabetically.

--- policy.tf
+++ policy.tf
@@ -4,10 +4,10 @@ module "policy_condenser" {
   policy_type = "${var.policy_type}"
 
   input_policies = [
-    "${data.aws_iam_policy_document.cloudcraft_access.*.json}"
     "${data.aws_iam_policy_document.apigateway_full_access.*.json}",
     "${data.aws_iam_policy_document.apigateway_read.*.json}",
     "${data.aws_iam_policy_document.apigateway_write.*.json}",
+    "${data.aws_iam_policy_document.cloudcraft_access.*.json}",
     "${data.aws_iam_policy_document.cloudcraft_access.*.json}",
     "${data.aws_iam_policy_document.dynamodb_full_access.*.json}",
     "${data.aws_iam_policy_document.dynamodb_list.*.json}",
@@ -22,9 +22,9 @@ module "policy_condenser" {
     "${data.aws_iam_policy_document.s3_list.*.json}",
     "${data.aws_iam_policy_document.s3_readonly.*.json}",
     "${data.aws_iam_policy_document.s3_write.*.json}",
+    "${data.aws_iam_policy_document.sqs_full_access.*.json}",
     "${data.aws_iam_policy_document.sqs_list_queues.*.json}",
     "${data.aws_iam_policy_document.sqs_readonly.*.json}",
     "${data.aws_iam_policy_document.sqs_write.*.json}",
-    "${data.aws_iam_policy_document.sqs_full_access.*.json}",
   ]
 }
