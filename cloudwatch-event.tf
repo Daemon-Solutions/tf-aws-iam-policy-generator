9091728cdc9699a302dae4ef5746bd7f2833a433
Gonzalo Fernandez Ordas <gonzalo.fernandezordas@dae.mn>
2021-03-10 11:38:23 +0000

    SD-610. Cloudwatch Alerts

--- cloudwatch-event.tf
+++ cloudwatch-event.tf
@@ -2,6 +2,7 @@ variable "cloudwatch_event_full_access" {
   description = "Boolean indicating whether to give Full Access to CloudWatch Events."
   default     = false
 }
+
 variable "cloudwatch_event_full_access_resources" {
   description = "A list of CloudWatch Event resources to which the user has Full Access too."
   type        = "list"
